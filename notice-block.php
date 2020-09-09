85637fb3ca43bf59ed4101ae14807b0d49a4007e
Tasnim Alam <tasnimalamcse@gmail.com>
2020-09-09 16:30:57 +0600

    Update to veresion 1.0.1

--- notice-block.php
+++ notice-block.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:     Notice
  * Description:     Put Spotlight On News, Announcements & Let The Visitors Find It Easily 
- * Version:         0.1.0
+ * Version:         1.0.1
  * Author:          The WordPress Contributors
  * License:         GPL-2.0-or-later
  * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
