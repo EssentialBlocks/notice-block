8b1342db53cc97e328976fc882afc48ae89435c9
hz-tyfoon <md.tifun@gmail.com>
2021-04-15 16:00:24 +0600

    css : editor panel width reducing on tab/mobile button clicking animated by transitioned

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'f25a1f6fcc4dd4451a3cc680c192c84a');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '8d9f38d65530620d501f6ec9f3b2c078');
