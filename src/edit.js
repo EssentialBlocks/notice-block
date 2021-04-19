39514eaf3f7cd3f14a3e90ac4992143804e2ec9f
hz-tyfoon <md.tifun@gmail.com>
2021-04-19 09:56:29 +0600

    explanatory comment added

--- src/edit.js
+++ src/edit.js
@@ -51,7 +51,7 @@ export default function Edit(props) {
 
 		paddingUnit,
 
-		paddingTop = 0,
+		paddingTop = 0, // padding attributes fallbacked to 0 when undefined. Because padding style is done in one line (see inside 'wrapperStylesDesktop') rather than 4 line so, it cannot be undefined there otherwise it'll be invalid css string.
 		paddingRight = 0,
 		paddingBottom = 0,
 		paddingLeft = 0,
