f66a8929bec2fdb75817c8e604e150c8096b5199
Priyo Mukul <priyomukul@gmail.com>
2021-05-06 16:40:49 +0600

    git modules updated

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'ea553da45916f69c480a5fb1fa4563d0');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '077e4640a6da0e4e394a293cf5200dc5');
