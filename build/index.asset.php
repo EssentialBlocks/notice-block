d4cfc2b8db3d57036a30bbca6fb25fa50e332a0a
hz-tyfoon <md.tifun@gmail.com>
2021-05-19 17:53:02 +0600

    runned yarn build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '1a54e06f83cbb51276b7b13d0f9c4bb4');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'bcaef471350d466d6bb57ef6d4517c9e');
